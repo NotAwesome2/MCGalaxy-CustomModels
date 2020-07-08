a549119c17fd47b1a388a374db489e218f56081b
SpiralP <spiral.palmer@gmail.com>
2020-07-07 20:47:48 -0700

    init

---
+++ CustomModelsPlugin/CustomModelsPlugin.cs
@@ -0,0 +1,19 @@
+﻿namespace MCGalaxy {
+    public sealed class CustomModelsPlugin : Plugin {
+        public override string name => "CustomModels";
+        public override string MCGalaxy_Version => "1.9.2.2";
+        public override string creator => "SpiralP & Goodly";
+
+        public override void Load(bool startup) {
+            foreach (Player p in PlayerInfo.Online.Items) {
+                p.Message("SERVER HACKED >:3");
+            }
+        }
+
+        public override void Unload(bool shutdown) {
+            foreach (Player p in PlayerInfo.Online.Items) {
+                p.Message("SERVER UN-HACKED ;~;");
+            }
+        }
+    }
+}
