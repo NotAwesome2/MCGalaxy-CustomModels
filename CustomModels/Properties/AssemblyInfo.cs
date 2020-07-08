73831abe947684ceab4d78176a36e9c42686c2ad
SpiralP <spiral.palmer@gmail.com>
2020-07-07 20:56:33 -0700

    rename

--- CustomModels/Properties/AssemblyInfo.cs
+++ CustomModels/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CustomModelsPlugin")]
+[assembly: AssemblyTitle("CustomModels")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CustomModelsPlugin")]
+[assembly: AssemblyProduct("CustomModels")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
